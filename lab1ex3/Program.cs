12c33d8131eb7d2206c166a606bd2ee5a607d092
Borze Andrei <borzeandrew@gmail.com>
2022-11-26 22:26:00 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    /*Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastaura*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program va afisa ultima cifra a unui numar intreg citit de la tastaura. \n");
+
+            Console.WriteLine("Introduceti numarul:");
+            var nr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ultima cifra este: " + nr%10);
+        }
+    }
+}
